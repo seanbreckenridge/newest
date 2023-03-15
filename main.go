772b678ca786a5116c6338001d25d5c624223fd2
Sean Breckenridge <seanbrecke@gmail.com>
2023-03-15 14:54:13 -0700

    prefix all errors with newest:

--- main.go
+++ main.go
@@ -43,10 +43,10 @@ Optional arguments:`)
 	case 1:
 		dir = flag.Arg(0)
 	default:
-		return nil, errors.New("newest: Provided too many positional arguments")
+		return nil, errors.New("provided too many positional arguments")
 	}
 	if *ignoreHidden && runtime.GOOS == "windows" {
-		return nil, errors.New("newest: Not able to check if files are hidden on windows")
+		return nil, errors.New("not able to check if files are hidden on windows")
 	}
 	return &NewestConfig{
 		includeDirs:  *includeDirs,
@@ -86,7 +86,7 @@ func newestPath(conf *NewestConfig) (os.FileInfo, error) {
 	}
 	// didn't find any files with this pattern (i.e. -include-dirs)
 	if recent == nil {
-		return nil, fmt.Errorf("newest: Could not find any matching files in %s", conf.dir)
+		return nil, fmt.Errorf("could not find any matching files in %s", conf.dir)
 	} else {
 		return recent, nil
 	}
@@ -108,7 +108,7 @@ func newest() (string, error) {
 func main() {
 	result, err := newest()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		fmt.Fprintf(os.Stderr, "newest: %s\n", err.Error())
 		os.Exit(1)
 	}
 	fmt.Println(result)
